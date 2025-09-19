026666d58a9dc0e00bd3fa8ae130ff3561d39e3c
Cezary Suchorski <cezary.suchorski@gmail.com>
2025-09-19 13:47:13 +0200

    Small work in progress changes to the VideoTile css

--- src/MeetClone.Client/src/components/MeetingPage/VideoTile.styled.tsx
+++ src/MeetClone.Client/src/components/MeetingPage/VideoTile.styled.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
 export const VideoTileStyled = styled.div`
-  width: 200px;
-  height: 150px;
-  border: 1px solid black;
+    max-width: 200px;
+    max-height: 150px;
+    border: 1px solid black;
+
+    & > video {
+        max-width: 100%;
+        max-height: 100%;
+    }
 `;
